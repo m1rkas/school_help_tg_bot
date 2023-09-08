d2933e6f04073cfed17caa94af332457a2112b1f
m1rkas <martem2108@gmail.com>
2023-09-08 19:18:28 +0300

    adding timetable and lessons schedule

--- school_help_bot.py
+++ school_help_bot.py
@@ -25,6 +25,95 @@ async def start_command(message: types.Message):
     await message.answer(greeting, reply_markup=kb_start)
 
 
+@dp.message_handler(Text(equals="Розклад"))
+async def schedule_command(message: types.Message):
+    await message.answer(schedule_text, reply_markup=kb_schedule)
+
+
+@dp.message_handler(Text(equals="Розклад дзвінків"))
+async def timetable_button(message: types.Message):
+    await message.answer(timetable_text, reply_markup=kb_timetable)
+
+@dp.message_handler(Text(equals="1 урок"))
+async def lesson1_command(message: types.Message):
+    await message.answer(lesson_1, parse_mode="html")
+
+
+@dp.message_handler(Text(equals="2 урок"))
+async def lesson2_command(message: types.Message):
+    await message.answer(lesson_2, parse_mode="html")
+
+
+@dp.message_handler(Text(equals="3 урок"))
+async def lesson3_command(message: types.Message):
+    await message.answer(lesson_3, parse_mode="html")
+
+
+@dp.message_handler(Text(equals="4 урок"))
+async def lesson4_command(message: types.Message):
+    await message.answer(lesson_4, parse_mode="html")
+
+
+@dp.message_handler(Text(equals="5 урок"))
+async def lesson5_command(message: types.Message):
+    await message.answer(lesson_5, parse_mode="html")
+
+
+@dp.message_handler(Text(equals="6 урок"))
+async def lesson6_command(message: types.Message):
+    await message.answer(lesson_6, parse_mode="html")
+
+
+@dp.message_handler(Text(equals="7 урок"))
+async def lesson7_command(message: types.Message):
+    await message.answer(lesson_7, parse_mode="html")
+
+
+@dp.message_handler(Text(equals="Всі уроки"))
+async def all_lesson_command(message: types.Message):
+    await message.answer(time_table, parse_mode="Html")
+
+
+@dp.message_handler(Text(equals="Розклад уроків"))
+async def lessons_schedule(message: types.Message):
+    await message.answer(lessons_orders_text, reply_markup=kb_lessons_schedule)
+
+
+@dp.message_handler(Text(equals=monday))
+async def monday_schedule(message: types.Message):
+    await message.answer(monday_sub, parse_mode="Html")
+
+
+@dp.message_handler(Text(equals=tuesday))
+async def tuesday_schedule(message: types.Message):
+    await message.answer(tuesday_sub, parse_mode="Html")
+
+
+@dp.message_handler(Text(equals=wednesday))
+async def wednesday_schedule(message: types.Message):
+    await message.answer(wednesday_sub, parse_mode="Html")
+
+
+@dp.message_handler(Text(equals=thursday))
+async def thursday_schedule(message: types.Message):
+    await message.answer(thursday_sub, parse_mode="Html")
+
+
+@dp.message_handler(Text(equals=friday))
+async def friday_schedule(message: types.Message):
+    await message.answer(friday_sub, parse_mode="Html")
+
+
+@dp.message_handler(Text(equals=saturday))
+async def saturday_schedule(message: types.Message):
+    await message.answer(saturday_sub, parse_mode="Html")
+
+
+@dp.message_handler(Text(equals="Розклад на весь тиждень"))
+async def full_week_schedule(message: types.Message):
+    await message.answer(full_lessons_schedule, parse_mode="Html")
+
+
 @dp.message_handler(Text(equals="Книги"))
 async def books_command(message: types.Message):
     await message.answer(books, reply_markup=kb_books)
@@ -32,7 +121,7 @@ async def books_command(message: types.Message):
 
 
 @dp.message_handler(Text(equals=alg))
-async def send_ald_book_command(message: types.document):
+async def send_alg_book_command(message: types.document):
     await bot.send_document(message.chat.id, alg_book)
 
 
@@ -71,9 +160,9 @@ async def send_chem_book_command(message: types.Message):
     await bot.send_document(message.chat.id, chem_book, caption="Це повне лайно")
 
 
-@dp.message_handler(Text(equals=geo))
-async def send_geo_book_command(message: types.Message):
-    await bot.send_document(message.chat.id, geo_book)
+@dp.message_handler(Text(equals=geography))
+async def send_geography_book_command(message: types.Message):
+    await bot.send_document(message.chat.id, geography_book)
 
 
 @dp.message_handler(Text(equals=world_hist))
@@ -89,6 +178,16 @@ async def send_ukr_hist_book_command(message: types.Message):
 @dp.message_handler(Text(equals=it))
 async def send_it_book_command(message: types.Message):
     await bot.send_document(message.chat.id, it_book)
+
+
+@dp.message_handler(Text(equals=juris))
+async def send_juris_book_command(message: types.Message):
+    await bot.send_document(message.chat.id, jurisprudence_book)
+
+
+@dp.message_handler(Text(equals=health))
+async def send_health_book_command(message: types.Message):
+    await bot.send_document(message.chat.id, health_book)
 
 
 @dp.message_handler(Text(equals=exit))
